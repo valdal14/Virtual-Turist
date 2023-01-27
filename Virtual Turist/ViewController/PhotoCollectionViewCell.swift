1e7719f0fb25f68726e867d41fd8b5e1e5abf0f7
Valerio DAlessio <u105550@canda.com>
2023-01-27 08:08:29 +0100

    Added CollectionView

---
+++ Virtual Turist/ViewController/PhotoCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoCollectionViewCell.swift
+//  Virtual Turist
+//
+//  Created by Valerio D'ALESSIO on 27/1/23.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    
+	@IBOutlet weak var photoImage: UIImageView!
+}
